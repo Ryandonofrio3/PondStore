dad2b1148f51041b3bf074c413dcf3e588a76868
Rydonofrio3 <ryandonofrio@gmail.com>
2023-01-17 00:01:12 -0800

    a

--- Pondstore.py
+++ Pondstore.py
@@ -1,6 +1,6 @@
 import csv
 
-with open('Products.csv', 'r') as file:
+with open('templates/Products.csv', 'r') as file:
     reader = csv.reader(file)
     for row in reader:
         print(row)
@@ -10,10 +10,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    with open('Products.csv', 'r') as file:
+    with open('/templatesProducts.csv', 'r') as file:
         reader = csv.reader(file)
         products = list(reader)
-    return render_template('index.html', products=products)
+    return render_template('templates/index.html', products=products)
 
 if __name__ == '__main__':
     app.run(debug=True)
